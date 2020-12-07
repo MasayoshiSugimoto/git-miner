a42b29d7158827fbaf8f7d8b3ac43949acdb4043
Masayoshi Sugimoto <sugimoto.massayoshi@gmail.com>
2020-12-07 23:18:50 +0900

    Refactoring

--- gitlogparser/parser_test.go
+++ gitlogparser/parser_test.go
@@ -1,6 +1,9 @@
 package gitlogparser
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestConvert(t *testing.T) {
 	const rawLogs = `commit c929894b81644c44800f805d352a93b644e2cc90
@@ -21,7 +24,33 @@ committer Masayoshi Sugimoto <sugimoto.massayoshi@gmail.com> 1606838128 +0900
 `
 
 	parser := LogParser{}
-	consumeLogs(&parser)
+
+	expecteds := []struct {
+		commit    string
+		author    string
+		timestamp int
+		timezone  string
+		log       string
+	}{
+		{
+			"c929894b81644c44800f805d352a93b644e2cc90",
+			"Masayoshi Sugimoto",
+			1606838152,
+			"+0900",
+			"Adding source files",
+		},
+		{
+			"3c38f5da65dc3d31dc3a7c2594cd49597374987d",
+			"Masayoshi Sugimoto",
+			1606838128,
+			"+0900",
+			"First commit: Add module file",
+		},
+	}
+
+	for _, line := range strings.Split(rawLogs, "\n") {
+		parser.readLine([]byte(line))
+	}
 
 	t.Logf(`parser = %+v`, parser)
 
@@ -29,19 +58,19 @@ committer Masayoshi Sugimoto <sugimoto.massayoshi@gmail.com> 1606838128 +0900
 		t.Errorf("Got length: %v, instead of: %v", len(parser.commits), 2)
 	}
 
-	if parser.commits[0].commit != "c929894b81644c44800f805d352a93b644e2cc90" {
-		t.Errorf("Got commit: %v, instead of: %v", parser.commits[0].commit, "c929894b81644c44800f805d352a93b644e2cc90")
-	}
-	if parser.commits[0].author != "Masayoshi Sugimoto" {
-		t.Errorf("Got author: %v, instead of: %v", parser.commits[0].author, "Masayoshi Sugimoto")
-	}
-	if parser.commits[0].timestamp != 1606838152 {
-		t.Errorf("Got timestamp: %v, instead of: %v", parser.commits[0].timestamp, 1606838152)
+	for i, expected := range expecteds {
+		current := parser.commits[i]
+		if current.commit != expected.commit {
+			t.Errorf("Got commit: %v, instead of: %v", current.commit, expected.commit)
+		}
+		if current.author != expected.author {
+			t.Errorf("Got author: %v, instead of: %v", current.author, expected.commit)
+		}
+		if current.timestamp != expected.timestamp {
+			t.Errorf("Got timestamp: %v, instead of: %v", current.timestamp, expected.timestamp)
+		}
 	}
 
-	if parser.commits[1].commit != "3c38f5da65dc3d31dc3a7c2594cd49597374987d" {
-		t.Errorf("Got commit: %v, instead of: %v", parser.commits[1].commit, "3c38f5da65dc3d31dc3a7c2594cd49597374987d")
-	}
 	if parser.current != 1 {
 		t.Errorf("Got current: %v, instead of: %v", parser.current, 1)
 	}
